666429f22578504bea9d8a43536316e49cb394da
heixinx <laoxiwushen@gmail.com>
2025-04-06 02:22:47 +0800

    Create zh_CN.lua
    Add Chinese localization

---
+++ localization/zh_CN.lua
@@ -0,0 +1,63 @@
+return {
+    misc = {
+        dictionary = {
+            carto_settings_compact_deck = "紧凑牌组",
+            carto_settings_deck_view = "牌组视图",
+            carto_settings_jokers = "小丑牌",
+            carto_settings_flames = "火焰效果",
+            carto_settings_other = "其他设置",
+            carto_settings_keybinds = "快捷键",
+
+            carto_compact_deck_enabled = "限制牌堆卡牌数量",
+            carto_compact_deck_visible_cards = "卡牌上限 ",
+
+            carto_deck_view_stack_enabled = "堆叠牌组视图卡牌",
+            carto_deck_view_hide_drawn_cards = "隐藏已抽取卡牌",
+            carto_deck_view_stack_modifiers = "堆叠所有修饰牌",
+            carto_deck_view_stack_chips = "堆叠不同筹码值",
+            carto_deck_view_stack_x_color = "颜色 ",
+            carto_deck_view_stack_background_opacity = "背景透明度 ",
+            carto_deck_view_stack_pos = "对齐方式 ",
+            carto_deck_view_stack_pos_vertical_options = {
+                "顶部",
+                "居中",
+                "底部"
+            },
+            carto_deck_view_stack_pos_horizontal_options = {
+                "左侧",
+                "中间",
+                "右侧"
+            },
+            carto_deck_view_unique_cards = "特殊卡牌：",
+
+            carto_draw_non_essential_shaders = "渲染非必要着色器",
+            carto_improved_hand_sorting = "优化手牌排序",
+            carto_dynamic_hand_align = "动态手牌对齐（支持超大牌型）",
+            carto_hide_tags = "隐藏标签",
+            carto_hide_consumables = "隐藏消耗品",
+            carto_hide_deck = "隐藏牌组",
+            carto_hide_jokers = "隐藏小丑牌",
+
+            carto_flames_intensity_min = "最低强度 ",
+            carto_flames_intensity_max = "最高强度 ",
+            carto_flames_relative_intensity = "火焰强度与当前盲注关联",
+            carto_flames_intensity_vanilla = "忽略强度极值",
+            carto_flames_gasoline_title = "效果预览",
+            carto_flames_gasoline = "",
+            carto_flames_volume = "火焰音量 ",
+            
+            carto_jokers_controls_buttons = "显示小丑牌区域按钮",
+            carto_jokers_controls_show_after = "当小丑牌总数超过 时显示按钮",
+            carto_jokers_hide = "隐藏",
+            carto_jokers_show = "显示",
+            carto_jokers_zoom = "缩放",
+
+            carto_waiting_keybind = "等待按键输入...",
+            carto_kb_hide_joker =  "隐藏小丑牌",
+            carto_kb_toggle_tags = "切换标签显示",
+            carto_kb_toggle_consumables = "切换消耗品显示",
+            carto_kb_toggle_jokers = "切换小丑牌显示",
+            carto_kb_toggle_jokers_buttons = "切换小丑牌按钮显示",
+        }
+    }
+}
